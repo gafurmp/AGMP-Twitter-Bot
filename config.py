34c35b8e3cfee3e9ab18ddff83269f5ba1683701
Abdul Gafoor MP <gafurmp@users.noreply.github.com>
2020-06-07 09:07:32 +0200

    addde new configuration parameters.

--- config.py
+++ config.py
@@ -13,3 +13,7 @@ Parameters
 '''
 LOGLVL = INFO
 RANDOM_TWEET_TXT_FILE = 'QuranQ.txt' # Your random tweet text file, one quote/ info per line formatting!
+RANDOM_TWEET_HOUR = 7
+RANDOM_TWEET_MINUTE = 0
+RANDOM_TWEET_SEC = 0
+RANDOM_TWEET_MSEC = 0
